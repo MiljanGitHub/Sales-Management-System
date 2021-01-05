3eb38410ddca9c4822829509a75cecdaed34a104
miljan_novisad <miljan.puletic@zesium.com>
2021-01-05 19:18:51 +0100

    initial commit

---
+++ src/main/java/sales/management/system/SalesManagenemtSystemApplication.java
@@ -0,0 +1,13 @@
+package sales.management.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesManagenemtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesManagenemtSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sales/management/system/SalesManagenemtSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package sales.management.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesManagenemtSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
